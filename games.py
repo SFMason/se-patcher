4bd42862b01fb414695c6c41c78f34e8e28aa2f6
Spenser Mason <mason2371@gmail.com>
2020-06-09 19:18:27 -0400

    Wrote games module for defining supported games and autodetecting them

---
+++ games.py
@@ -0,0 +1,26 @@
+"""
+games.py - List of all the games this patcher supports.
+"""
+
+import os
+from typing import Tuple, List, Optional, TypedDict
+from collections import namedtuple
+
+# cSpell: words TESV, skse, nvse, fose
+
+Game = namedtuple("Game", ["exe", "se_exe"])
+
+FALLOUT_4 = Game("Fallout4", "f4se")
+SKYRIM = Game("TESV", "skse")
+FALLOUT_NEW_VEGAS = Game("FalloutNV", "nvse")
+FALLOUT_3 = Game("Fallout3", "fose")
+
+
+def detect_game(path: Optional[str] = None) -> Optional[Game]:
+    """Search the current directory for a supported game."""
+    if path is None:
+        path = os.getcwd()
+    for game in (FALLOUT_4, SKYRIM, FALLOUT_NEW_VEGAS, FALLOUT_3):
+        if os.path.isfile(os.path.join(path, "{}.exe".format(game.exe))):
+            return game
+    return None
